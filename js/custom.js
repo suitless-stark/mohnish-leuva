ae01732dad1f2c2d000025408b97f13263053fc0
Mohnish Leuva <leuvamohnish9@gmail.com>
2021-09-12 20:34:13 +1200

    loader update

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,24 @@
 (function() {
 
-  // $(window).load(function() {
-  //   // fades out when the page is fully loaded
-  //   $('.lds-ellipsis').delay(2000).fadeOut(500);
+  $(window).on('load', function() {
+    // fades out when the page is fully loaded
+    $('.loader').delay(2000).fadeOut(500);
+  });
+
+  // window.addEventListener('load', function(event) {
+  //   console.log('page is fully loaded');
+  //   function fade() {
+  //     setTimeout(fade, 50000);
+  //     document.querySelector('.loader').style.opacity = 0;
+  //   }
+  //   fade();
   // });
 
   // typewriter animation
   function typeWriter() {
     var i = 0;
     var txt = 'a junior web & graphic designer';
-    var speed = 90;
-
-    console.log(txt);
+    var speed = 150;
 
     function typeWriterEffect() {
       if (i < txt.length) {
